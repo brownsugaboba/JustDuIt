ac28902873a9c8ba4213daea8ac51d6176eb04d7
LuigiAmadeo <83432525+LuigiAmadeo@users.noreply.github.com>
2021-06-21 22:57:58 +0700

    rapihin

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,7 @@ public class Main {
 		//LoginView log = new LoginView();
 		//EmployeeHandler.viewManageEmployeeForm();
 		EmployeeHandler.showLoginView();
-		//halo
-		//testing
-		//hai fon
-		//asdfasdfasdf
-		//hai fon woi fon j
-		//Alfon Ketik
-		// woi 
-		// Testing Testing
+
 	}
 
 	public static void main(String[] args) {
